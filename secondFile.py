11252d7e7c3cbdaa5d704410d8a15ee2f2e5ab2b
enginartut <engin.artut@gmail.com>
2022-11-11 22:08:35 +0300

    second commit on my laptop

---
+++ secondFile.py
@@ -0,0 +1,4 @@
+merhaba dunyali. aslinda burada olmasÕ gereken her ne varsa burada yaziyorum. benim deilk denemem olan budurum oldukca sasirtiici
+benide blmgiyordum bdurumu . nasÕldÕ da boyle bir duruma geldm
+acan olabailir mi diye yaziyorum baklim ne olacak?
+olur mu dersin?
